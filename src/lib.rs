8e2feb9319bd2b69a90b86818c13b75c41882578
Scotch101Tape <66388250+Scotch101Tape@users.noreply.github.com>
2021-11-09 01:00:43 +0000

    Buidling basic api

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,54 @@
+#![no_std]
+
+struct Task {
+
+}
+
+struct TaskQue {
+
+}
+
+impl TaskQue {
+    fn new() -> TaskQue {
+        TaskQue {
+
+        }
+    }
+}
+
+struct Scheduler {
+    que: TaskQue,
+}
+
+impl Scheduler {
+    fn new() -> Scheduler {
+        let que = TaskQue::new();
+
+        Scheduler {
+            que
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+    
+    #[test]
+    fn example_of_api() {
+        let scheuler = Scheduler::new();
+
+        scheuler.defer(Task::new_task(|| {
+
+        }));
+
+        scheuler.defer(Task::new_loop(|| {
+
+        }));
+
+        scheuler.run();
     }
 }
